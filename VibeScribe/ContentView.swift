3809fa61634accc34a59f364ce217e978478e9fc
Pavel Frankov <pavel@frankov.ru>
2025-04-13 20:13:03 +0300

    Initial Commit

---
+++ VibeScribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VibeScribe
+//
+//  Created by Frankov Pavel on 13.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VibeScribe/VibeScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeScribeApp.swift
+//  VibeScribe
+//
+//  Created by Frankov Pavel on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct VibeScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
